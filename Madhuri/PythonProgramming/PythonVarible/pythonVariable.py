a9f51a7677d7e1721ded04430bbd3d328aaa2ebb
Your Name <you@example.com>
2025-01-15 22:17:53 +0530

    Adding file from pycharm

---
+++ Madhuri/PythonProgramming/PythonVarible/pythonVariable.py
@@ -0,0 +1,10 @@
+a = 5
+# a : variable
+# = : assignment operator
+# 10 : value assign to the variable
+
+print(a)  # 5
+print(id(a))  # 140733707905592
+
+b = 15
+print(b, id(b))  # 15 140733707905912
