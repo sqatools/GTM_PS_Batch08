26b9f6ba93817881e3d87fb36d1ff279007f50f4
mrpramodmr <pramodmr159@gmail.com>
2025-01-27 22:41:13 +0530

    Calculator problem solved

--- Pramod/Python_Programming/Home_Work/Calculator 24_01_025.py
+++ Pramod/Python_Programming/Home_Work/Calculator 24_01_025.py
@@ -8,3 +8,21 @@ if decision ==4 : division num1, num2
 if decision > 4 : invalid input
 
 """
+num1 =10
+num2 =20
+decision=5
+
+if decision ==1:
+    print ("adding two numbers 10+20=", num1+num2)
+elif decision ==2:
+    print("subtraction two numbers 10-20=", num1-num2)
+
+elif decision ==3:
+    print("Multiplication two numbers 10*20=", num1*num2)
+elif decision ==4:
+    print("Division two numbers 10%20=", num1%num2)
+else:
+    decision >4
+    print("invalid output")
+
+
