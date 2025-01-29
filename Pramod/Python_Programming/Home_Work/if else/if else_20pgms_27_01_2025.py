eeb9d184fe685c3896451197d78e4ec11b6abc5b
mrpramodmr <pramodmr159@gmail.com>
2025-01-29 17:29:38 +0530

    if else _20 programs _ 2 done

--- Pramod/Python_Programming/Home_Work/if else/if else_20pgms_27_01_2025.py
+++ Pramod/Python_Programming/Home_Work/if else/if else_20pgms_27_01_2025.py
@@ -5,5 +5,12 @@ if n%3==0:
 else:
     print("the given number is not divisible by 3")
 print ("_"*50)
+
+
 #2)If else program to get all the numbers divided by 3 from 1 to 30.
 for i in(1,30,1):
+    if i%3==0:
+        print (i)
+print ("_"*50)
+
+#3)else program to assign grades as per total marks.
