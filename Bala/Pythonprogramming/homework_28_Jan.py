239c36af8890eba51cdd86cd0e892ec047a2b3ba
Balsu89 <balsu89@gmail.com>
2025-01-29 20:14:10 +0530

    Nested if

---
+++ Bala/Pythonprogramming/homework_28_Jan.py
@@ -0,0 +1,9 @@
+# Program to [ront value which are divisible by 5 and 7 from 1 to 50
+# for i in range (1,51):
+#     if i%5==0 and i%7==0:
+#         print(i)
+
+# Program to print square of even number and cube of odd from 1 to 20
+for i in range (1,21):
+    if i%2==0:
+        print('i',2**i)
