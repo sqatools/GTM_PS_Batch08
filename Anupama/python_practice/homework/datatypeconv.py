67ce12318cb5c891283d269a3e0b28123f14d3ca
singhanu07 <singhanu07@gmail.com>
2025-01-31 21:12:01 +0530

    adding file from pycharm

--- Anupama/python_practice/homework/datatypeconv.py
+++ Anupama/python_practice/homework/datatypeconv.py
@@ -507,3 +507,19 @@ print("_"*50)
 ### bool -> tuple ###  Conversion is not possible
 ### bool -> dict ###  Conversion is not possible
 ### bool -> set ###  Conversion is not possible
+
+
+********adding a string and integer*********
+num_string = '12'
+num_integer = 23
+
+print("Data type of num_string before Type Casting:",type(num_string))
+
+num_string = int(num_string)
+
+print("Data type of num_string after Type Casting:",type(num_string))
+
+num_sum = num_integer + num_string
+
+print("Sum:",num_sum)
+print("Data type of num_sum:",type(num_sum))
