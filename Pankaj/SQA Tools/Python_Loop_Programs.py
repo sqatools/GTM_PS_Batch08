f4e963b7fb7b0a9faf4a3658d6e0397129218a5e
Pankaj Prasad <150281644+pankajprasad30@users.noreply.github.com>
2025-02-03 12:37:24 +0530

    Update Python_Loop_Programs.py

--- Pankaj/SQA Tools/Python_Loop_Programs.py
+++ Pankaj/SQA Tools/Python_Loop_Programs.py
@@ -99,6 +99,37 @@ for char in HumInput:
 print("Digits :", digit)
 print("Character :", character)
 
+# 10). Python for loop program to print the alphabet pattern ‘O’ using python.
+# Output:
+#    ***
+# *       *
+# *       *
+# *       *
+# *       *
+# *       *
+#    ***
 
+print("_" * 60)
+for i in range(1, 8):
+    for j in range(1, 8):
+        if (i == 1 and j == 3) or (i == 1 and j == 4) or (i == 1 and j == 5):
+            print("*", end=" ")
+        elif (i == 2 and j == 1) or (i == 2 and j == 7):
+            print("*", end=" ")
+        elif (i == 3 and j == 1) or (i == 3 and j == 7):
+            print("*", end=" ")
+        elif (i == 4 and j == 1) or (i == 4 and j == 7):
+            print("*", end=" ")
+        elif (i == 5 and j == 1) or (i == 5 and j == 7):
+            print("*", end=" ")
+        elif (i == 6 and j == 1) or (i == 6 and j == 7):
+            print("*", end=" ")
+        elif (i == 7 and j == 3) or (i == 7 and j == 4) or (i == 7 and j == 5):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+# 11). Python Loops program to print all natural numbers from 1 to n using a while loop in python.
 
 
