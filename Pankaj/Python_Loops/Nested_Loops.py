b2bbc52170e55ef0248e1a5feeef9b3f46f9116f
Pankaj Prasad <150281644+pankajprasad30@users.noreply.github.com>
2025-02-03 10:07:48 +0530

    Update Nested_Loops.py

--- Pankaj/Python_Loops/Nested_Loops.py
+++ Pankaj/Python_Loops/Nested_Loops.py
@@ -136,6 +136,18 @@ print("_"*70)
 
 """
 
-for i in range(6):
-    for j in range(6):
-        print("*")
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == 1 and j == 3:
+            print("@", end=" ")
+        elif (i == 2 and j == 2) or (i == 2 and j == 4):
+            print("@", end=" ")
+        elif (i == 3 and j == 1) or (i == 3 and j == 5):
+            print("@", end=" ")
+        elif (i == 4 and j == 2) or (i == 4 and j == 4):
+            print("@", end=" ")
+        elif (i == 5 and j == 3) or (i == 5 and j == 3):
+            print("@", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
