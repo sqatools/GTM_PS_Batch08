321ddb3eba99c4d1bbc3f98c5345bad8485bf72b
Bi12Python <xovek11875@bitvoo.com>
2025-02-05 22:42:52 +0530

    Python String slicing session2

--- Deepesh/PythonProgramming/Python_String/Python_string_practice.py
+++ Deepesh/PythonProgramming/Python_String/Python_string_practice.py
@@ -83,8 +83,108 @@ str4 = "Learning Python"
 4. get given output  : "LLLearning Pythonnn"
 """
 
+print("_"*50)
+##################
+# Rule2 :  str[ : end index]
+# Default initial index will be zero
+
+str_b = "Programming"
+print(str_b[:6]) # Progra
+
+print(str_b[:-3]) # Programm
+print(str_b[:-1]) # Programmin
+print(str_b[:]) # Programming
+print(str_b[:0]) # blank
+
+print("_"*50)
+##################
+# Rule3 :  str[ start index: ]
+# Default end index would be end of the string.
+
+str_c = "Good Evening"
+print(str_c[5:]) # Evening
+
+print(str_c[-9:]) # d Evening
 
 
+print("_"*50)
+##################
+# Rule4 :  str[ start index: end index : step]
+# ->  substring output will contain start index value and exclude end index
+# ->  substring output will depend on step value defined.
+# ->  start index, end index and step value could be +ve and -ve.
+# ->  substring output could be reverse as well
+
+
+str_x = "IndiaIsBestCountry"
+
+# [+ve:+ve:+ve]
+print(str_x[2:10:1]) #
+
+print(str_x[2:13:3]) # diaIsBes
+
+print(str_x[0:17:2]) # Idasetonr
+
+# [+ve:-ve:+ve]
+print(str_x[1:-1:1]) # ndiaIsBestCountr
+str_len  = len(str_x)
+print(str_len)
+print(str_x[1:len(str_x):1]) # ndiaIsBestCountry
+
+# If we dont defined the end index, default end index would be end of string
+print(str_x[1::1]) # ndiaIsBestCountry
+
+
+# [-ve:-ve:+ve]
+str_y = "PythonIsGreat"
+print(str_y[-8:-1:1]) # nIsGrea
+
+# [-ve:-ve:-ve]
+print(str_y[-10:-1:-1]) # blank
+print(str_y[-1:-10:-1]) # taerGsIno
+print(str_y[-1:-len(str_y):-1]) # taerGsInohty
+print(str_y[-1:-len(str_y)-1:-1]) # taerGsInohtyP
+
+#str_y[-1:-13:-1]
+
+# Notes: when we try to find the substring with the help slicing rule
+# Then we have to look to next value of start index, if next index value is in between the range
+# of start index and end index, then it will return output, otherwise it will return blank.
+
+# Notes : Negative index movement consider as reverse substring
+#       : Positive index movement consider as positive substring
+
+##################################
+# Rule 5:   str[:: step]
+# -> IF step is positive:
+#     -> In this rule if we don't define the start index, then default start index will 0
+#     -> In this rule if we don't define the end index, then default end index will end of string.
+#     -> It will read the sub-string from left to right
+
+# -> IF step is Negative:
+#    -> In this rule if we don't define the start index, then default initial index will -1
+#    -> In this rule if we don't define the end index, then default end index will, start of string.
+#    -> It will read the sub-string from right to left
+
+print("_"*50)
+# with +ve step
+str_z = "WeAreLearningPython"
+print(str_z[::1])  # WeAreLearningPython
+print(str_z[::2])  # WAeerigyhn
+print(str_z[:8:2]) # WAee
+print(str_z[2::2]) # Aeerigyhn
+
+print("_"*50)
+# with -ve step
+str_p = "LearningSlicing"
+print(str_p[::-1]) # gnicilSgninraeL
+#-1:-len(str)-1:-1]
+
+
+print(str_p[::]) # LearningSlicing
+print(str_p[:-1:]) # LearningSlicin
+print(str_p[-1:len(str_p):1]) # g
+print(str_p[-1::]) # g
 
 
 
