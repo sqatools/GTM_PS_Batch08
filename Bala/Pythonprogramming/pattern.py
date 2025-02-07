063aee4adb93f1a256c677643032bed4e71b180f
Balsu89 <balsu89@gmail.com>
2025-02-07 21:10:49 +0530

    Nested if

---
+++ Bala/Pythonprogramming/pattern.py
@@ -0,0 +1,9 @@
+# ##### Pattern printing ########
+# for i in range(1,6,1):
+#     print(i*"*")
+# for i in range(4,-1,-1):
+#     print(i*"*")
+
+for i in range(0, 11):
+    if i ==3 and i ==6:
+        print(i, end=" ")
