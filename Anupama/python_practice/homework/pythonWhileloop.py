c685be8b37509f6b743dfd3529b1f2649e74a584
singhanu07 <singhanu07@gmail.com>
2025-02-10 22:39:27 +0530

    adding file from pycharm

--- Anupama/python_practice/homework/pythonWhileloop.py
+++ Anupama/python_practice/homework/pythonWhileloop.py
@@ -2,3 +2,13 @@ for i in range(1, 5):
     for j in range(i):
         print(i, end=' ')
     print()
+print("-"*50)
+
+#add all the elements of the list using while**
+list1 = [2,5,8,0,1]
+count = 0
+total = 0
+while count < len(list1):
+    total += list1[count]
+    count += 1
+print(total)
