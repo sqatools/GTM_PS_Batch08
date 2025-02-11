45079eddd42897879a0bbf5b60112a3a975d0cb2
ChhaviGupta93 <chhavi.gupta.canada@gmail.com>
2025-02-11 10:00:58 -0500

    new programs

---
+++ Chhavi/Slicing in String.py
@@ -0,0 +1,25 @@
+#Rule1
+#str[start index : end index]
+str_a = "My name is Chhavi Gupta"
+
+#reading the whole sentence
+print(str_a[0:23])
+print(str_a[-12:-6])
+
+
+
+#repeat your name twice
+print(str_a[11:17] * 2)
+print(str_a[11:17], " ", str_a[11:17])
+print(str_a[-12:-6] * 2)
+
+#Read your name with +ve and -ve index value
+print(str_a[11:-6])
+
+
+
+
+
+
+
+
