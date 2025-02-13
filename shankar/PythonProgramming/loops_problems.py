196eb96c9a9c712ccbc73328d3707f60def98fe9
sankar1990 <shankarbujji1990@gmail.com>
2025-02-13 09:19:09 +0530

    Practice problems

--- shankar/PythonProgramming/loops_problems.py
+++ shankar/PythonProgramming/loops_problems.py
@@ -45,6 +45,11 @@ print("-" * 50)
 Input: “python”
 Output : “python”
 """
+word = "python"
+empty_str = ""
+for i in word:
+    empty_str += i
+print(empty_str)
 
 print("-" * 50)
 """
@@ -53,16 +58,46 @@ Input : (1, 2, 3, 4, 5, 6, 7, 8, 9)
 Output :
 Number of even numbers: 4
 Number of odd numbers: 5
+"""
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+even_nums_count = 0
+odd_nums_count = 0
+for i in numbers:
+    if i % 2 == 0:
+        even_nums_count += 1
+    else:
+        odd_nums_count += 1
+print(f"Number of even numbers: {even_nums_count}")
+print(f"Number of odd numbers: {odd_nums_count}")
 
 print("-" * 50)
+
+"""
 5). Write a program that prints all the numbers from 0 to 6 except 3 and 6 using python.
-
+"""
+for i in range(7):
+    if i == 3 or i == 6:
+        pass
+    else:
+        print(i, end=" ")
+print()
 print("-" * 50)
-
+"""
 6). Write a program to get the Fibonacci series between 0 to 20 using python.
 Fibonacci Series : 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
+"""
+limit = 20
+a, b = 0, 1  # first 2 number in series
+for i in range(limit):
+    if i > limit:
+        break
+    for j in range(1):
+        a, b = b, a + b
+    print(a, end=" ")
 
+print()
 print("-" * 50)
+"""
 7). Write a program that iterates the integers from 1 to 30 using python. For multiples of three print “Fizz” instead of the number and for multiples of five print “Buzz”.
 For numbers that are multiples of both three and five print “FizzBuzz”.
 
