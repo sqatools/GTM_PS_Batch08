644a7601f979879f73292064f0c30ab9c74b22d2
kartikkumar1 <kartiksudani@gmail.com>
2025-02-13 21:28:43 +0530

    work

---
+++ Kartik/PythonProgramming/Python_Basic_Program/Python_Basic_Program1.py
@@ -0,0 +1,16 @@
+print('_'*50)
+# 1). Python Program to add two integer values.
+num1 = 10
+num2 = 20
+
+Addition  = num1 + num2
+print("Addition of two num :", Addition)
+
+print('_'*50)
+# 2). Python Program to subtract two integer values.
+
+num1 = 10
+num2 = 20
+
+subtract  = num2 - num1
+print("subtraction of two num :", subtract)
