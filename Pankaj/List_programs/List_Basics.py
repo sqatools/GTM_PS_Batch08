1f55ef4c21ca3c315da1eefb55a330dc3837f646
pankajprasad30 <pankajprasad30@gmail.com>
2025-02-13 16:12:36 +0530

    commit on 13/02/2026, from PyCharm

---
+++ Pankaj/List_programs/List_Basics.py
@@ -0,0 +1,7 @@
+"""
+--> List is mutable data type, so we can update its value
+--> List can contain all data types, eg: int, float,bool,set, tuple,string, list,dict
+--> List follows positive and negative indexing
+--> we can define list under square bracket
+"""
+list1 = [1, 2.2, "Pankaj", 'P', True, (2, 3.3, "Prasad"[1, 2, 3]), (1, 2, 3, 4), {'a':'Darshika', 'b':""}]
