bdae11c091ed9b36a0478eb32d6fbf3cbc688001
kgshema <ghemaster@gmail.com>
2025-02-25 20:57:51 +0530

    Adding Python set basics

--- Hema/PythonProgramming/Python_Set/Python_set_basics.py
+++ Hema/PythonProgramming/Python_Set/Python_set_basics.py
@@ -184,3 +184,107 @@ print("_" * 50)
 set_12.intersection_update(set_11)
 print("set11 :", set_11)  # {'b', 'c', 'a'}
 print("set_12:", set_12)  # {'r', 's', 'q', 'b', 'p', 'c', 'a'}
+
+#24th Feb2025
+print("*"*50)
+
+######################
+# difference method() : This method return set of difference value from set1 to set2.
+set_13 = {'a', 'b', 'c', 'd', 'e', 'f', 3, 5}
+set_14 = {'p', 'q', 'a', 'r', 'b', 'c', 's'}
+
+diff_output = set_13.difference(set_14)
+print("diff output :", diff_output)
+# diff output : {3, 5, 'd', 'e', 'f'}
+
+diff_output2 = set_14.difference(set_13)
+print("diff output2 :", diff_output2)
+# {'q', 'p', 's', 'r'}
+
+print("_" * 50)
+######################
+# difference_update method() : This method update the target set with difference output.
+
+# this will update set_13 with difference value.
+set_13.difference_update(set_14) # {3, 5, 'd', 'e', 'f'}
+print("set_13 :", set_13) # {3, 5, 'd', 'e', 'f'}
+print("set_14:", set_14) # {'c', 'q', 'b', 'r', 'a', 'p', 's'}
+
+
+print("_" * 50)
+######################
+# symmetric_difference() : This method return the difference value from both the sets
+
+
+set_15 = {1, 4, 7, 8, 'a', 'b'}
+set_16 = {'p', 'q', 1, 4, 7, 8}
+sym_diff = set_15.symmetric_difference(set_16)
+print("sym diff :", sym_diff) # {'b', 'q', 'a', 'p'}
+
+# symmetric_difference_update
+set_15.symmetric_difference_update(set_16)
+print("set_15 :", set_15) # {'b', 'p', 'a', 'q'}
+print("set_16 :", set_16) # {1, 4, 'p', 7, 8, 'q'}
+
+print("_" * 50)
+###############################
+# superset and subset method : Superset method check the given value in set, has all the value
+#                          from other child set as well, then it super set.
+#                          ->  When we have big set values and compare with small set, which
+#                          ->  can include child set values, thn it is known as subset.
+
+set_17 = {1, 4, 7, 8, 'a', 'b', 'P', 'Q', 'R'}
+set_18 = {'P', 'Q', 'R'}
+set_19 = {'a', 'b', 8, 10}
+
+print("set_17 is super set :", set_17.issuperset(set_18)) # True
+print("set_18 is subset of set_17 :", set_18.issubset(set_17)) # True
+print("set_19 is subset of set_17 :", set_19.issubset(set_17)) # False
+
+print("_"*50)
+#####################################
+
+set_17 = {1, 4, 7, 8, 'a', 'b', 'P', 'Q', 'R'}
+set_18 = {'P', 'Q', 'R'}
+set_20 = {'P','Q', 'R'}
+print(set_18.issubset(set_20))
+print(set_18.issuperset(set_20))
+
+print("_"*50)
+##############################
+# isdisjoint method :  disjoint method check the both the set are completely different from each other.
+
+set_21 = {1, 4, 7, 8, 'a', 'b', 'P', 'Q', 'R'}
+set_22 = {'P', 'Q', 'R'}
+set_23 = {'A', 'B', 'C'}
+
+print(set_22.isdisjoint(set_21)) # False
+# set_23 has completely different value from target set.
+print(set_22.isdisjoint(set_23)) # True
+
+
+print("_"*50)
+##############################
+# copy method  :  This method help to copy content from on set to another.
+
+# shallow copy
+set_24 = {5, 7, 9, 11}
+set_25 = set_24
+set_25.add(100)
+set_24.add(200)
+
+print("set_24 :", set_24 ) # {100, 5, 7, 200, 9, 11}
+print("set_25 :", set_25 ) # {100, 5, 7, 200, 9, 11}
+
+
+# Deep Copy
+
+print("_"*50)
+set_27 = {11, 66, 88, 22, 26}
+
+set_26 = set_27.copy()
+set_26.add(400)
+
+print("set_26 :", set_26) # {400, 66, 22, 88, 26, 11}
+#
+print("set_27 :", set_27) # {66, 22, 88, 26, 11}
