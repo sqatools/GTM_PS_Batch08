218380213728c0956ee9fedd6a914bf7d8d1871e
Shilpi <mailtosjohri@gmail.com>
2025-02-25 10:40:51 -0600

    new code

---
+++ Shilpi/PythonProgramming_Shilpi/set_2_24_2025.py
@@ -0,0 +1,10 @@
+#Q3 write a python program to remove all the duplicate value from given dictionary values
+dict1 = {'a' : [3, 5, 7, 3, 7, 8],
+         'b' : 'Programming',
+         'c' : (4, 7, 9, 11, 5, 7, 11, 77, 88, 11),
+         'd' : [True, False, True, False]}
+dict2={}
+for k,v in dict1.items():
+    dict2[k]=set(v)
+print(dict2)
+
