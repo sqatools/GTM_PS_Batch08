7f4297efdc696831af3548eceb7a12ad88928ed5
kgshema <ghemaster@gmail.com>
2025-02-27 20:58:51 +0530

    Adding Python set

---
+++ Hema/PythonProgramming/HomeWork/python_set/Python_set_20_programs.py
@@ -0,0 +1,185 @@
+
+ch = int(input("Enter number from 1 to 20: "))
+
+if ch==1:
+    '''
+    1). Python program to create a set with some elements.
+    '''
+    set_var_1 = {1,2,3,4}
+    set_var_1.add(5)
+    print(set_var_1)
+
+elif ch==2:
+    '''
+2). Python program to add an element to a set.
+
+    '''
+    set_var_2= set()
+    print(type(set_var_2))
+    set_var_2.add(1)
+    print(set_var_2)
+    set_var_2.add(2)
+    print(set_var_2)
+
+elif ch==3:
+    '''
+3). Python program to remove an element from a set.
+'''
+    set_var_3 = {2,3,4,5,6}
+    print("original set: ", set_var_3)
+    set_var_3.remove(5)
+    print(set_var_3)
+
+
+elif ch==4:
+    '''
+4). Python program to find the length of a set.
+
+'''
+    set_var_4 = {1,2,3,4,5,6}
+    print("original set: ", set_var_4)
+    print("length of set: ",len(set_var_4))
+
+elif ch==5:
+    '''
+5). Python program to check if an element is present in a set.
+
+'''
+
+
+'''
+6). Python program to find the union of two sets.
+
+Solution
+7). Python program to find the intersection of two sets.
+
+Solution
+8). Python program to find the difference of two sets.
+
+Solution
+9). Python program to find the symmetric difference of two sets.
+
+Solution
+10). Python program to show if one set is a subset of another set.
+
+Solution
+11). Python program to check if two sets are disjoint.
+
+Solution
+12). Python program to convert a list to a set.
+
+Solution
+13). Python program to convert a set to a list.
+
+Solution
+14). Python program to find the maximum element in a set.
+
+Solution
+15). Python program to find the minimum element in a set.
+
+Solution
+16). Python program to find the sum of elements in a set.
+
+Solution
+17). Python program to find the average of elements in a set.
+
+Solution
+18). Python program to check if all elements in a set are even.
+
+Solution
+19). Python program to check if all elements in a set are odd.
+
+Solution
+20). Python program to check if all elements in a set are prime.
+
+Solution
+21). Python program to check if a set is a proper subset of another set.
+
+Solution
+22). Python program to find the cartesian product of two sets.
+
+Solution
+23). Python program to find the power set of a set.
+
+24). Python program to remove all elements from a set.
+
+Solution
+25). Python program to remove a random element from a set.
+
+Solution
+26). Python program to find the difference between two sets using the “-” operator.
+
+Solution
+27). Python program to find the intersection between two sets using the “&” operator.
+
+Solution
+28). Python program to find the union of multiple sets using the | operator.
+
+Solution
+29). Python program to find the symmetric difference of two sets using the “^” operator
+
+Solution
+30). Python program to check if a set is a superset of another set.
+
+Solution
+31). Python program to find the common elements between two sets.
+
+Solution
+32). Python program to remove a specific element from a set.
+
+Solution
+33). Python program to add multiple elements to a set.
+
+Solution
+34). Python program to remove multiple elements from a set.
+
+Solution
+35). Python program to check if a set is empty.
+
+Solution
+36). Python program to check if two sets are equal.
+
+Solution
+37). Python program to check if a set is a frozen set.
+
+Solution
+38). Python program to create a frozen set.
+
+Solution
+39). Python program to find the difference between multiple sets.
+
+Solution
+40). Python program to find the intersection between multiple sets.
+
+Solution
+41). Python program to check if any element in a set is a substring of a given string.
+
+Solution
+42). Python program to check if any element in a set is a prefix of a given string.
+
+43). Python program to check if any element in a set is a suffix of a given string.
+
+44). Python program to find the index of an element in a set.
+
+Solution
+45). Python program to convert a set to a dictionary with each element as key and value to an empty set.
+
+Solution
+46). Python program to create a set of even numbers from 1 to 20.
+
+Solution
+47). Python program to create a set of odd numbers from 1 to 20.
+
+Solution
+48). Python program to create a set of your favorite actors.
+
+Solution
+49). Python program to create a set of your favorite movies.
+
+Solution
+50). Python program to create two sets of books and find the intersection of sets.
+
+Solution
+51). Python program to find the longest word in a set.
+
+'''
