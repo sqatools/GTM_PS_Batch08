40f69e2798952463fd00dccd2569f63b983880fb
pankajprasad30 <pankajprasad30@gmail.com>
2025-03-03 12:26:53 +0530

    commit on 28/02/2026, from PyCharm

--- Pankaj/Python_SQA Tools/Python_String_Program.py
+++ Pankaj/Python_SQA Tools/Python_String_Program.py
@@ -27,7 +27,7 @@ occ = "da"
 print(str3.count(occ))
 
 print("_" * 70)
-#5. Python string program to test whether a passed letter is a vowel or consonant.
+#5). Python string program to test whether a passed letter is a vowel or consonant.
 str4 = "pankaj"
 for char in str4:
     if char == "a" or char == "e" or char == "i" or char == "u" or char == "o" or char == "u":
@@ -126,7 +126,7 @@ print(list1)
 
 
 print("_" * 70)
-#13.) Write a python program to replace the words “Java” with “Python” in the given string.
+#13). Write a python program to replace the words “Java” with “Python” in the given string.
 # Input = “JAVA is the Best Programming Language in the Market”
 # Output = “Python is the Best Programming Language in the Market”
 
@@ -142,90 +142,3 @@ print(" ".join(list1))
 print("_" * 70)
 #14.
 
-
-print("_" * 70)
-#8).Write a python program to get to swap the last character of a given string.
-#Input = “SqaTool”
-#Output = “IqaTooS”
-str7 = "sqatools"
-print(str7[-1]+str7[1:-1]+str7[0])
-
-print("_" * 70)
-#9). Write a python program to exchange the first and last character of each word from the given string.
-#Input = “Its Online Learning”
-#Output = “stI enlino gearninL”
-
-str8 = "Its Online Learning"
-list1 = str8.split(" ")
-for word in list1:
-    new_word = word[-1]+word[1:-1]+word[0]
-    index = list1.index(word)
-    list1[index] = new_word
-" ".join(list1)
-print(list1)
-
-print("_" * 70)
-#10.  Write a python to repeat vowels 3 times and consonants 2 times.
-#Input = “Sqa Tools Learning”
-#Ouput = “SSqqaaa TToooooollss LLeeeaaarrnniiinngg”
-str9 = "Sqa Tools Learning"
-vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-result = ""
-for char in str9:
-    if char in vowels:
-        result += char*3
-    else:
-        result += char*2
-print(result)
-
-print("_" * 70)
-#11. Write a python program to re-arrange the string.
-#Input = “Cricket Plays Virat”
-#Output = “Virat Plays Cricket”
-str10 = "Cricket Plays Virat"
-word = str10.split(" ")
-word.reverse()
-" ".join(word)
-print(word)
-
-
-print("_" * 70)
-#12. Write a python program to get all the digits from the given string.
-"""
-Sinak’s 1112 aim is to 1773 create a new generation of people who
-understand 444 that an organization’s 5324 success or failure is
-based on 555 leadership excellence and not managerial
-acumen
-"""
-# Output = [1112, 5324, 1773, 5324, 555]
-str11 = """
-Sinak’s 1112 aim is to 1773 create a new generation of people who
-understand 444 that an organization’s 5324 success or failure is
-based on 555 leadership excellence and not managerial
-acumen
-"""
-word = str11.split(" ")
-list1 = []
-for value in word:
-    if value.isdigit():
-        list1.append(value)
-
-print(list1)
-
-
-print("_" * 70)
-#13.) Write a python program to replace the words “Java” with “Python” in the given string.
-# Input = “JAVA is the Best Programming Language in the Market”
-# Output = “Python is the Best Programming Language in the Market”
-
-str13 = "JAVA is the Best Programming Language in the Market"
-list1 = str13.split(" ")
-for word in list1:
-    if word == "JAVA":
-        index = list1.index(word)
-        list1[index] = "Pyhton"
-
-print(" ".join(list1))
-
-print("_" * 70)
-#14.
