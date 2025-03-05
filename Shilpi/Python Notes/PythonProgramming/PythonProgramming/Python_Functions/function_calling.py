ff5d96761e56593e5754bbafc50ba1940f88a9d7
Shilpi <mailtosjohri@gmail.com>
2025-03-05 11:12:47 -0600

    new code

---
+++ Shilpi/Python Notes/PythonProgramming/PythonProgramming/Python_Functions/function_calling.py
@@ -0,0 +1,7 @@
+# import specific function
+# from python_function2 import get_even_odd
+# import all available variable/method/function/class
+from python_function2 import *
+
+get_even_odd(5, 7, 9, 11, 5, 23, 22)
+add(50, 70)
