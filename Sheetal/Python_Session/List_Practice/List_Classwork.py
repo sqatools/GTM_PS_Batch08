5a9116baffe3aa76a32fd42fe535f80072f2abbd
BachuSheetal <99080652+BachuSheetal@users.noreply.github.com>
2025-03-06 20:49:38 +0530

    Homework on File_Handling

--- Sheetal/Python_Session/List_Practice/List_Classwork.py
+++ Sheetal/Python_Session/List_Practice/List_Classwork.py
@@ -541,3 +541,50 @@ list_c = [55, 66, 33, 22, 12]
 #Q2 . write a python check value can be divide by 3 and 7
 list_d = [9, 42, 33, 21, 28, 25]
 # output : [42, 21]
+
+
+
+in_list_1 = [23, 2, 4, 6, 7]
+k = 6
+count = 0
+
+for i in range(0, len(in_list_1)):
+    if in_list_1[i] < k:
+        count += in_list_1[i]
+        j = i+1
+        while count < k:
+            count += in_list_1[j]
+            j += 1
+            if count == k:
+                print("True")
+                break
+            else:
+                count = 0
+    else:
+        continue
+
+for i in range(0, len(in_list_1)):
+    if in_list_1[i] < k:
+        count += in_list_1[i]
+        j = i+1
+        while count < k:
+            count += in_list_1[j]
+            j += 1
+            if count == k:
+                print("True")
+                break
+            else:
+                count = 0
+    else:
+        continue
+# This is the question:
+#
+# Given a list of non-negative numbers and a target integer k,
+# ,check if the array has a continuous subarray of size at least 2 that sums up to k
+# #Example 1:
+
+# Input: [23, 2,4, 6, 7], k=6
+#
+# Output: True
+#
+# Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
