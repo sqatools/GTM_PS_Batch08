7933e718852dfd3bd8fe73eed6a95e336ef78d3e
Bi12Python <xovek11875@bitvoo.com>
2025-03-12 22:08:48 +0530

    exception handling in Python

--- Deepesh/PythonProgramming/Exception_Handling/Python_exception_handling.py
+++ Deepesh/PythonProgramming/Exception_Handling/Python_exception_handling.py
@@ -1,17 +1,17 @@
-
 # Handle exeception erro msg
 def basic_exception_handling():
     try:
         num1 = 50
         num2 = "Hello"
-        print(num1+num2)
+        print(num1 + num2)
     except Exception as e:
         print(e)
         print("Can not add string with integer")
 
     print("Good Morning")
 
-#basic_exception_handling()
+
+# basic_exception_handling()
 
 
 # raise explicit exception. : When user wants to fail the program to explicitly with the exception error
@@ -22,7 +22,7 @@ def raise_explicit_exception():
     try:
         num1 = 50
         num2 = "Hello"
-        print(num1+num2)
+        print(num1 + num2)
     except Exception as e:
         print(e)
         print("Can not add string with integer")
@@ -31,7 +31,7 @@ def raise_explicit_exception():
     print("Good Morning")
 
 
-#raise_explicit_exception()
+# raise_explicit_exception()
 
 
 #########################
@@ -43,7 +43,7 @@ def try_except_else_block_handling(num1, num2):
     try:
         x = num1
         y = num2
-        print("addition :", x+y)
+        print("addition :", x + y)
     except Exception as e:
         print(e)
         print("Can not add string with integer")
@@ -55,7 +55,7 @@ def try_except_else_block_handling(num1, num2):
 # correct values
 # try_except_else_block_handling(10, 20)
 # incorrect values
-#try_except_else_block_handling("Hello", 20)
+# try_except_else_block_handling("Hello", 20)
 
 
 #########################
@@ -66,7 +66,7 @@ def try_except_else_finally_block_handling(num1, num2):
     try:
         x = num1
         y = num2
-        print("addition :", x+y)
+        print("addition :", x + y)
     except Exception as e:
         print(e)
         print("Can not add string with integer")
@@ -78,14 +78,14 @@ def try_except_else_finally_block_handling(num1, num2):
     finally:
         # This section will always execute
         for i in range(1, 11):
-            print(i, "*", num1, ":", i*num1)
+            print(i, "*", num1, ":", i * num1)
 
 
 # correct values
-#try_except_else_finally_block_handling(5, 6)
+# try_except_else_finally_block_handling(5, 6)
 
 # in-correct values
-#try_except_else_finally_block_handling(7, 'Hello')
+# try_except_else_finally_block_handling(7, 'Hello')
 
 
 ######################################
@@ -93,15 +93,15 @@ def try_except_else_finally_block_handling(num1, num2):
 
 def handle_multiple_exception(num1, num2, num3, filepath):
     try:
-        addition  = num1+num2
+        addition = num1 + num2
         print("addition :", addition)
 
         # multiplication
-        mul = num1*num2
+        mul = num1 * num2
         print("multiplication :", mul)
 
         # division:
-        divide = num2//num3
+        divide = num2 // num3
         print("division error :", divide)
 
         # read file
@@ -124,5 +124,58 @@ def handle_multiple_exception(num1, num2, num3, filepath):
         raise e
 
 
+# handle_multiple_exception(5, 10, 2, 'read_data.txt')
 
-handle_multiple_exception(5, 10, 2, 'read_data.txt')
+def handle_nested_level_exception(num1, num2, num3, filepath):
+    try:
+        addition = num1 + num2
+        print("addition :", addition)
+
+        # multiplication
+        mul = num1 * num2
+        print("multiplication :", mul)
+        try:
+            # division:
+            divide = num2 // num3
+            print("division error :", divide)
+
+            # read file
+            with open(filepath, "r") as file:
+                data = file.read()
+                print(data)
+
+            # compare value
+            assert num1 == num2
+        except Exception as e:
+            print("Inner exception :", e)
+            #raise
+        finally:
+            print("Inner finally")
+
+    except TypeError:
+        print("Outer Exception : both the values should be integer")
+
+    finally:
+            print("outer finally")
+
+
+#handle_nested_level_exception(20, 20, 30, "read_data2.txt")
+
+
+##################################
+# custom exception
+
+class custom_exception(Exception):
+    def __init__(self):
+        print("Raising custom exception")
+
+
+def use_custom_exception():
+    for i in range(10):
+        if i == 5:
+            raise custom_exception
+        else:
+            continue
+
+
+use_custom_exception()
