e613e081728ed929cef2ac3f6eee4b59c0d09bf9
kgshema <ghemaster@gmail.com>
2025-03-28 19:59:05 +0530

    Selenium_basic_concept

---
+++ Hema/PythonProgramming/Python_OOPS/class_import/train_booking.py
@@ -0,0 +1,8 @@
+from selenium_code import SeleniumCode
+
+class TrainBooking(SeleniumCode):
+
+
+    def book_a_Train(self):
+        print("Method from Train Booking class")
+        self.click_element()
