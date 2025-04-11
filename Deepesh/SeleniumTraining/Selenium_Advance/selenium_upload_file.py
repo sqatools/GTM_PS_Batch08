9078a4489355460603ed63108f376148ed348b61
Bi12Python <xovek11875@bitvoo.com>
2025-04-11 22:34:12 +0530

    Java script executor

---
+++ Deepesh/SeleniumTraining/Selenium_Advance/selenium_upload_file.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(15)
+
+driver.get("https://automationbysqatools.blogspot.com/2020/08/login.html")
+
+upload_element = driver.find_element(By.ID, "myFile")
+upload_element.send_keys(["E:\\Filesdata\\count_name.txt"])
+
+time.sleep(5)
+
+driver.find_element(By.XPATH, "(//input[@type='submit'])[1]").click()
+
+time.sleep(5)
+driver.close()
+
+
+
