4626b5823e6d7ffdc03b295c0ab7e0dead878a7b
kgshema <ghemaster@gmail.com>
2025-04-16 00:01:21 +0530

    Selenium_goibibo_home_work

--- Hema/SeleniumTraining/Selenium_Home_work/Selenium_goibibo_search_homework/goibibo_search_homework.py
+++ Hema/SeleniumTraining/Selenium_Home_work/Selenium_goibibo_search_homework/goibibo_search_homework.py
@@ -18,7 +18,7 @@ driver.implicitly_wait(10)
 alert = Alert(driver)
 def launch_url():
 
-    driver.get("https://www.goibibo.com/flights/?utm_source=bing&utm_medium=cpc&utm_campaign=DF-Brand-EM&utm_adgroup=Only%20Goibibo&utm_term=!SEM!DF!B!Brand!RSA!150035352!1211662109442708!!e!goibibo!c!")
+    driver.get("https://www.goibibo.com/")
     close_icon = driver.find_element(By.XPATH, "//span[@role='presentation']")
     close_icon.click()
     time.sleep(5)
@@ -51,48 +51,53 @@ def header_items():
 
     time.sleep(5)
 
-def bus_header_item():
+def bus_item():
 
     list_bus_names = []
     location_detail_list =[]
     bus_link = driver.find_element(By.XPATH, "//a[@href='/bus/']/span[1]")
     bus_link.click()
+
+    # From_bus_option
     from_source = driver.find_element(By.ID, "autosuggestBusSRPSrcHome")
     from_source.send_keys("Chennai")
 
-    action = ActionChains(driver)
-
     source_dropdown_values = driver.find_element(By.XPATH, "//div[@id='downshift-1-item-1']//span")
-    #print(len(source_dropdown_values))
-    # for source_dropdown_value in source_dropdown_values:
-    #     print(source_dropdown_value, end=" ")
-    #source_dropdown_values.click()
     source_dropdown_values.click()
 
     time.sleep(10)
-    # source_click.click()
-    # #print(source_click.text)
+
+    #To_bus_option
     to_source = driver.find_element(By.ID, "autosuggestBusSRPDestHome")
     to_source.send_keys("Bangalore")
     time.sleep(2)
 
     to_source_dropdown_values = driver.find_element(By.XPATH, "//div[@id='downshift-2-item-4']//span")
-    #action.move_to_element(to_source_dropdown_values).click()
     to_source_dropdown_values.click()
     time.sleep(5)
-    #travel_dat = driver.find_element(By.XPATH, "//div[@class='SearchWidgetstyles__DateTabs-sc-1mr4hwz-4 gKiMdw']/span[@class='active']")
+
+    #Travel date 'Tommorrow' option
+
     travel_dat = driver.find_element(By.XPATH, "//span[contains(text(),'Tomorrow')]")
     time.sleep(5)
     travel_dat.click()
     time.sleep(5)
+
+    #Search_button_option
     search_button=driver.find_element(By.XPATH, "//button[@data-testid='searchBusBtn']")
     search_button.click()
     time.sleep(5)
 
-    #bus_type=driver.find_element(By.XPATH, "//div[contains(text(),'AC')]")
+    #to get the total number of bus names
     bus_names = driver.find_elements(By.XPATH, "//div[@class='SrpActiveCardstyles__LayoutFirstDiv-sc-yk1110-4 ijRmXM']/p[1]")
     print("\n", len(bus_names))
 
+
+    #xpath:
+    #dropping_point: //p[text()='Dropping Point']//following-sibling::div//p[text()='ATTIBELLI']//parent::div//preceding-sibling::label
+    #boarding_point: //p[text()='Dropping Point']//following-sibling::div//p[text()='ATTIBELLI']//parent::div//preceding-sibling::label
+
+    #printing the bus names
     for bus_name in bus_names:
         #print("\n",bus_name.text)
         list_bus_names.append(bus_name.text)
@@ -104,6 +109,8 @@ def bus_header_item():
 
     # for seat in total_seats:
     #     print(seat.text, end="\n")
+
+    #based on the bus name clicking seat option
 
     select_bus_name = 'CIT TRAVELS'
 
@@ -120,6 +127,8 @@ def bus_header_item():
     location_details = driver.find_elements(By.XPATH, "//p[@class='location']")
     print(len(location_details))
 
+    #to get the location details
+
     for location_detail in location_details:
         #print("\n", location_detail.text)
         location_detail_list.append(location_detail.text)
@@ -128,33 +137,141 @@ def bus_header_item():
     location_detail = driver.find_element(By.XPATH, "//p[@class='location']")
     radio_button = driver.find_element(By.XPATH, "//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
 
-    if point_headers.text == 'Boarding Point':
-        #location_detail = driver.find_element(By.XPATH, "//p[@class='location']")
-        if location_detail.text == 'Koyembedu':
-            #radio_button = driver.find_element(By.XPATH, "//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
-            print(radio_button.is_selected())
-            radio_button.click()
-            print(radio_button.is_selected())
-    #dropping_point = driver.find_element(By.XPATH, "//p[@class='boxHeaders']")
+    boarding_point = driver.find_element(By.XPATH, "//p[text()='Boarding Point']//following-sibling::div//p[text()='Tharamani']//parent::div//preceding-sibling::label")
+    boarding_point.click()
 
-    if point_headers.text == 'Dropping Point':
-        if location_detail.text == 'Electronicity':
-            print(radio_button.is_selected())
-            radio_button.click()
-            print(radio_button.is_selected())
+    time.sleep(5)
 
-    #//div[@class="BusBerthstyles__BusOutline-sc-rfhcvl-0 bDTuH"]/aside[2]
+    #facing issue in dropping point
+    #dropping_point = driver.find_element(By.XPATH, "//p[text()='Dropping Point']//following-sibling::div//p[text()='Electronic City']//parent::div//preceding-sibling::label")
+    dropping_point = driver.find_element(By.XPATH, "//p[text()='Dropping Point']//following-sibling::div//p[text()='Bommasandra']//parent::div//preceding-sibling::label")
+    dropping_point.click()
+
+    time.sleep(2)
+
     seat_fill = driver.find_element(By.XPATH, "//div[@class='SeatWithTooltipstyles__BusSeat-sc-dkrka-2 kBvUB']")
     seat_fill.click()
 
+    time.sleep(2)
     continue_button = driver.find_element(By.XPATH, "//button[@class='SelectSeatTabContentstyles__Button-sc-aw7o7o-9 cZetwz']")
     continue_button.click()
 
     time.sleep(15)
 
+
+# def bus_header_item():
+#
+#     list_bus_names = []
+#     location_detail_list =[]
+#     bus_link = driver.find_element(By.XPATH, "//a[@href='/bus/']/span[1]")
+#     bus_link.click()
+#     from_source = driver.find_element(By.ID, "autosuggestBusSRPSrcHome")
+#     from_source.send_keys("Chennai")
+#
+#     action = ActionChains(driver)
+#
+#     source_dropdown_values = driver.find_element(By.XPATH, "//div[@id='downshift-1-item-1']//span")
+#     #print(len(source_dropdown_values))
+#     # for source_dropdown_value in source_dropdown_values:
+#     #     print(source_dropdown_value, end=" ")
+#     #source_dropdown_values.click()
+#     source_dropdown_values.click()
+#
+#     time.sleep(10)
+#     # source_click.click()
+#     # #print(source_click.text)
+#     to_source = driver.find_element(By.ID, "autosuggestBusSRPDestHome")
+#     to_source.send_keys("Bangalore")
+#     time.sleep(2)
+#
+#     to_source_dropdown_values = driver.find_element(By.XPATH, "//div[@id='downshift-2-item-4']//span")
+#     #action.move_to_element(to_source_dropdown_values).click()
+#     to_source_dropdown_values.click()
+#     time.sleep(5)
+#     #travel_dat = driver.find_element(By.XPATH, "//div[@class='SearchWidgetstyles__DateTabs-sc-1mr4hwz-4 gKiMdw']/span[@class='active']")
+#     travel_dat = driver.find_element(By.XPATH, "//span[contains(text(),'Tomorrow')]")
+#     time.sleep(5)
+#     travel_dat.click()
+#     time.sleep(5)
+#     search_button=driver.find_element(By.XPATH, "//button[@data-testid='searchBusBtn']")
+#     search_button.click()
+#     time.sleep(5)
+#
+#     #bus_type=driver.find_element(By.XPATH, "//div[contains(text(),'AC')]")
+#     bus_names = driver.find_elements(By.XPATH, "//div[@class='SrpActiveCardstyles__LayoutFirstDiv-sc-yk1110-4 ijRmXM']/p[1]")
+#     print("\n", len(bus_names))
+#
+#
+#     #xpath:
+#     #dropping_point: //p[text()='Dropping Point']//following-sibling::div//p[text()='ATTIBELLI']//parent::div//preceding-sibling::label
+#     #boarding_point: //p[text()='Dropping Point']//following-sibling::div//p[text()='ATTIBELLI']//parent::div//preceding-sibling::label
+#
+#     for bus_name in bus_names:
+#         #print("\n",bus_name.text)
+#         list_bus_names.append(bus_name.text)
+#
+#     print(list_bus_names)
+#
+#     total_seats = driver.find_elements(By.XPATH, "//span[@class='SrpActiveCardstyles__PayTxtSpan-sc-yk1110-32 dPUgol']")
+#     print("\n", len(total_seats))
+#
+#     # for seat in total_seats:
+#     #     print(seat.text, end="\n")
+#
+#     select_bus_name = 'CIT TRAVELS'
+#
+#     if select_bus_name in list_bus_names:
+#         select_seat_option = driver.find_element(By.XPATH, "//span[@class='SrpActiveCardstyles__PayTxtSpan-sc-yk1110-32 dPUgol']")
+#         select_seat_option.click()
+#
+#     point_headers = driver.find_element(By.XPATH, "//p[@class='boxHeaders']")
+#     print(point_headers.text)
+#
+#     radio_buttons = driver.find_elements(By.XPATH, "//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
+#     print(len(radio_buttons))
+#
+#     location_details = driver.find_elements(By.XPATH, "//p[@class='location']")
+#     print(len(location_details))
+#
+#     for location_detail in location_details:
+#         #print("\n", location_detail.text)
+#         location_detail_list.append(location_detail.text)
+#     print(location_detail_list)
+#
+#     location_detail = driver.find_element(By.XPATH, "//p[@class='location']")
+#     radio_button = driver.find_element(By.XPATH, "//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
+#
+#     if point_headers.text == 'Boarding Point':
+#         print("Boarding point")
+#         location_detail = driver.find_element(By.XPATH, "//p[@class='location']")
+#         if location_detail.text == 'Koyembedu':
+#             radio_button = driver.find_element(By.XPATH, "//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
+#             print(radio_button.is_selected())
+#             radio_button.click()
+#             print(radio_button.is_selected())
+#     #dropping_point = driver.find_element(By.XPATH, "//p[@class='boxHeaders']")
+#
+#     if point_headers.text == 'Dropping Point':
+#         location_detail = driver.find_element(By.XPATH, "//p[@class='location']")
+#         if location_detail.text == 'Electronicity':
+#             radio_button = driver.find_element(By.XPATH,"//span[@class='RadioButtonstyles__Checkmark-sc-wz601o-1 bFGqGl']")
+#             print(radio_button.is_selected())
+#             radio_button.click()
+#             print(radio_button.is_selected())
+#
+#     #//div[@class="BusBerthstyles__BusOutline-sc-rfhcvl-0 bDTuH"]/aside[2]
+#     seat_fill = driver.find_element(By.XPATH, "//div[@class='SeatWithTooltipstyles__BusSeat-sc-dkrka-2 kBvUB']")
+#     seat_fill.click()
+#
+#     continue_button = driver.find_element(By.XPATH, "//button[@class='SelectSeatTabContentstyles__Button-sc-aw7o7o-9 cZetwz']")
+#     continue_button.click()
+#
+#     time.sleep(15)
+
 launch_url()
 notification_prompt()
 header_items()
-bus_header_item()
+#bus_header_item()
+bus_item()
 driver.close()
 
