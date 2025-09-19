1c038744c5d45b346001be07b3d824c8b53e9e2a
admin <sidhantkashyap04@gmail.com>
2025-09-19 15:43:13 +0530

    updated committed on 19 sept

--- Sidhant/Python_Programming/HybridFrameWork/tests/Flightpage/test_flight_booking_page_new.py
+++ Sidhant/Python_Programming/HybridFrameWork/tests/Flightpage/test_flight_booking_page_new.py
@@ -15,18 +15,18 @@ class TestFlightBooking:
         self.util = Utils()
         self.json_data = self.util.read_json( json_file_path )
 
-    def test_src_dest_city(self):
-        # launch goibibo website
-        self.fp.close_popup()
-        time.sleep( 6 )
-        self.fp.select_source_city( "Mumbai, India" )
-        time.sleep( 10)
-        self.fp.select_dest_city( "New Delhi, India" )
-        time.sleep( 10 )
+    # def test_src_dest_city(self):
+    #     # launch goibibo website
+    #     self.fp.close_popup()
+    #     time.sleep( 6 )
+    #     self.fp.select_source_city( "Mumbai, India" )
+    #     time.sleep( 10)
+    #     self.fp.select_dest_city( "New Delhi, India" )
+    #     time.sleep( 10 )
 
-    def test_select_travel_data(self):
-        self.fp.select_travel_date( self.json_data['travel_date'] )
-        time.sleep(20)
+    # def test_select_travel_data(self):
+    #     self.fp.select_travel_date( self.json_data['travel_date'] )
+    #     time.sleep(20)
     def test_login_functionality(self,get_user_cred):
         user_creds = get_user_cred
         self.driver.get("https://practicetestautomation.com/practice-test-login/")
